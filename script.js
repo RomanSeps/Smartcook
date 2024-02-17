900a7adece01c2db4c3aa3c73b56724c3b0ebc1c
RomanSeps <96355457+RomanSeps@users.noreply.github.com>
2024-02-17 16:37:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,193 +1,167 @@
-// Definuj proměnnou, pro změnu viditelnosti
 var labelsVisible = false;
-var button = document.getElementById("btn")
-var button1 = document.getElementById("btn1")
-var button2 = document.getElementById("btn2")
-var button3 = document.getElementById("btn3")
-var button4 = document.getElementById("btn4")
+var buttonFilled = false;
+var all = document.getElementById("all");
+var dish = document.getElementById("dish")
+var recipe = document.getElementById("recipe")
+var difficulty = document.getElementById("difficulty")
+var price = document.getElementById("price")
+var tolerance = document.getElementById("tolerance")
 
+function demo1(){
+    var labels1 = document.querySelectorAll('.labels1')
 
-var card1 = document.getElementById("card1")
-var card2 = document.getElementById("card2")
-var card3 = document.getElementById("card3")
+    if(!labelsVisible){
+        dish.style.backgroundColor = 'aqua';
+        buttonFilled = true;
 
-// Funkce pro viditelnost
-function demo() {
-    var labels = document.querySelectorAll('.label'); // Sežeň všechny "label"
-    
-    if (!labelsVisible) {
-        button.innerText = "- Dish category"
-        button1.style.display = 'none'
-        button2.style.display = 'none'
-        button3.style.display = 'none'
-        button4.style.display = 'none'
-        card1.style.marginTop = '-5%'
-        card2.style.marginTop = '-5%'
-        card3.style.marginTop = '-5%'
-
-        labels.forEach(function(label) {
+        labels1.forEach(function(label) {
             label.style.display = 'block'; // Ukaž labely
+        });
+        labelsVisible = true;
+
+    }else{
+        
+        dish.style.background = 'none';
+        buttonFilled = false;
+
+        labels1.forEach(function(label) {
+            label.style.display = 'none'; 
             
         });
-        labelsVisible = true; // Změna proměnné
-    } else {
-        button.innerText = "+ Dish category"
-        button1.style.display = 'block'
-        button2.style.display = 'block'
-        button3.style.display = 'block'
-        button4.style.display = 'block'
-        card1.style.marginTop = '-40%'
-        card2.style.marginTop = '-40%'
-        card3.style.marginTop = '-40%'
-        labels.forEach(function(label) {
-            label.style.display = 'none'; // Schovej všechny labely
-        });
-        labelsVisible = false; // Změna proměnné
+
+        labelsVisible = false;
     }
 }
 
-function demo1() {
-    var labels = document.querySelectorAll('.label1'); // Sežeň všechny "label"
+function demo2(){
+    var labels1 = document.querySelectorAll('.labels2')
 
-    if (!labelsVisible) {
-    button1.innerText = "- Recipe category"
-    button.style.display = 'none'
-    button2.style.display = 'none'
-    button3.style.display = 'none'
-    button4.style.display = 'none'
-    card1.style.marginTop = '-5%'
-    card2.style.marginTop = '-5%'
-    card3.style.marginTop = '-5%'
-        labels.forEach(function(label) {
+    if(!labelsVisible){
+        recipe.style.backgroundColor = 'aqua';
+        buttonFilled = true;
+
+        labels1.forEach(function(label) {
             label.style.display = 'block'; // Ukaž labely
         });
-        labelsVisible = true; // Změna proměnné
-    } else {
-    button1.innerText = "+ Recipe category"
-    button.style.display = 'block'
-    button2.style.display = 'block'
-    button3.style.display = 'block'
-    button4.style.display = 'block'
-    card1.style.marginTop = '-40%'
-    card2.style.marginTop = '-40%'
-    card3.style.marginTop = '-40%'
-        labels.forEach(function(label) {
-            label.style.display = 'none'; // Schovej všechny labely
-        });
-        labelsVisible = false; // Změna proměnné
-    }
-}
+        labelsVisible = true;
 
-function demo2() {
-    var labels = document.querySelectorAll('.label2'); // Sežeň všechny "label"
-    
-    if (!labelsVisible) {
-    button2.innerText = "- Difficulty"
-    button.style.display = 'none'
-    button1.style.display = 'none'
-    button3.style.display = 'none'
-    button4.style.display = 'none'
-    card1.style.marginTop = '-5%'
-    card2.style.marginTop = '-5%'
-    card3.style.marginTop = '-5%'
-        labels.forEach(function(label) {
-            label.style.display = 'block'; // Ukaž labely
+    }else{
+        
+        recipe.style.background = 'none';
+        buttonFilled = false;
+
+        labels1.forEach(function(label) {
+            label.style.display = 'none'; 
             
         });
-        labelsVisible = true; // Změna proměnné
-    } else {
-    button2.innerText = "+ Difficulty"
-    button.style.display = 'block'
-    button1.style.display = 'block'
-    button3.style.display = 'block'
-    button4.style.display = 'block'
-    card1.style.marginTop = '-40%'
-    card2.style.marginTop = '-40%'
-    card3.style.marginTop = '-40%'
-        labels.forEach(function(label) {
-            label.style.display = 'none'; // Schovej všechny labely
-        });
-        labelsVisible = false; // Změna proměnné
+
+        labelsVisible = false;
     }
 }
 
-function demo3() {
-    var labels = document.querySelectorAll('.label3'); // Sežeň všechny "label"
-    
-    if (!labelsVisible) {
-    button3.innerText = "- Price"
-    button.style.display = 'none'
-    button1.style.display = 'none'
-    button2.style.display = 'none'
-    button4.style.display = 'none'
-    card1.style.marginTop = '-5%'
-    card2.style.marginTop = '-5%'
-    card3.style.marginTop = '-5%'
-        labels.forEach(function(label) {
+function demo3(){
+    var labels1 = document.querySelectorAll('.labels3')
+
+    if(!labelsVisible){
+        difficulty.style.backgroundColor = 'aqua';
+        buttonFilled = true;
+
+        labels1.forEach(function(label) {
             label.style.display = 'block'; // Ukaž labely
+        });
+        labelsVisible = true;
+
+    }else{
+        
+        difficulty.style.background = 'none';
+        buttonFilled = false;
+
+        labels1.forEach(function(label) {
+            label.style.display = 'none'; 
             
         });
-        labelsVisible = true; // Změna proměnné
-    } else {
-    button3.innerText = "+ Price"
-    button.style.display = 'block'
-    button1.style.display = 'block'
-    button2.style.display = 'block'
-    button4.style.display = 'block'
-    card1.style.marginTop = '-40%'
-    card2.style.marginTop = '-40%'
-    card3.style.marginTop = '-40%'
-        labels.forEach(function(label) {
-            label.style.display = 'none'; // Schovej všechny labely
-        });
-        labelsVisible = false; // Změna proměnné
+
+        labelsVisible = false;
     }
 }
 
-function demo4() {
-    var labels = document.querySelectorAll('.label4'); // Sežeň všechny "label"
-    
-    if (!labelsVisible) {
-    button4.innerText = "- Tolerance"
-    button.style.display = 'none'
-    button1.style.display = 'none'
-    button2.style.display = 'none'
-    button3.style.display = 'none'
-    card1.style.marginTop = '-5%'
-    card2.style.marginTop = '-5%'
-    card3.style.marginTop = '-5%'
-        labels.forEach(function(label) {
+function demo4(){
+    var labels1 = document.querySelectorAll('.labels4')
+
+    if(!labelsVisible){
+        price.style.backgroundColor = 'aqua';
+        buttonFilled = true;
+
+        labels1.forEach(function(label) {
             label.style.display = 'block'; // Ukaž labely
+        });
+        labelsVisible = true;
+
+    }else{
+        
+        price.style.background = 'none';
+        buttonFilled = false;
+
+        labels1.forEach(function(label) {
+            label.style.display = 'none'; 
             
         });
-        labelsVisible = true; // Změna proměnné
-    } else {
-    button4.innerText = "+ Tolerance"
-    button.style.display = 'block'
-    button1.style.display = 'block'
-    button2.style.display = 'block'
-    button3.style.display = 'block'
-    card1.style.marginTop = '-40%'
-    card2.style.marginTop = '-40%'
-    card3.style.marginTop = '-40%'
-        labels.forEach(function(label) {
-            label.style.display = 'none'; // Schovej všechny labely
-        });
-        labelsVisible = false; // Změna proměnné
+
+        labelsVisible = false;
     }
 }
 
-const filter = document.querySelector('[id = "type1"]');
-let check = [];
+function demo5(){
+    var labels1 = document.querySelectorAll('.labels5')
 
-filter.addEventListener('search', (e) => {
-    e.preventDefault;
+    if(!labelsVisible){
+        tolerance.style.backgroundColor = 'aqua';
+        buttonFilled = true;
 
-    document.querySelectorAll('[type ="checkbox"]').forEach(item => {
-        if (item.checked === true){
-            check.push(item.value);
-        }
-    })
+        labels1.forEach(function(label) {
+            label.style.display = 'block'; // Ukaž labely
+        });
+        labelsVisible = true;
+
+    }else{
+        
+        tolerance.style.background = 'none';
+        buttonFilled = false;
+
+        labels1.forEach(function(label) {
+            label.style.display = 'none'; 
+            
+        });
+
+        labelsVisible = false;
+    }
+}
+
+const cardsContainer = document.querySelector('#card-container');
+
+const data = fetch('https://www.smartcook-project.eu/api/recipes')
+    .then((response) => response.json())
+    .then((characters) => console.log(characters.data));
+
+    function renderCharacters(characters) {
+        characters.forEach(character => {
+          const div = document.createElement('div');
+          const name = document.createElement('h2');
+          const difficulty = document.createElement('p')
+          const price = document.createElement('p')
+          div.classList = 'cards'
+          name.innerText = ` ${character.name}`
+          difficulty.innerText = `Difficulty: ${character.difficulty}`
+          price.innerText = `Price: ${character.price}`
+
+          div.appendChild(name)
+          div.appendChild(difficulty)
+          div.appendChild(price)
+          cardsContainer.appendChild(div)
 })
+};
 
-console.log(check);
+fetch('https://www.smartcook-project.eu/api/recipes')
+    .then((response) => response.json())
+    .then((characters) => renderCharacters(characters.data));
+
